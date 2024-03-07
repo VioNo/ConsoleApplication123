22c2baec73b5829e434af817131855c8538628bd
227136 <227136@KB37-1-309-C07>
2024-03-07 13:03:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication123/ConsoleApplication123.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+#include<string>
+#include<iomanip>
+#include<vector>
+#include<algorithm>
+#include <windows.h>
+using namespace std;
+	
+//struct Student{
+//	public:
+//		int id{};
+//		string fio{};
+//		string gruppa{};
+//    private:
+//		char pol{};
+//		int year{};
+//		int stependia{};
+class Student{
+public:
+		int id{};
+		string fio{};
+		string gruppa{};
+    private:
+		char pol{};
+		int year{};
+		int stependia{};
+public:
+
+	void Gender(char gen)
+	{
+		gen = pol;
+	}
+	char Gen(char gen)
+	{
+		return gen;
+	}
+	};
+int main() 
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Student Ivan;
+	Ivan.id = { 1 };
+	Ivan.fio = { "иванов иван" };
+	Ivan.gruppa = { "2исип-222" };
+
+	cout << Ivan.id << " " << Ivan.fio;
+	 
+	return 0;
+}
